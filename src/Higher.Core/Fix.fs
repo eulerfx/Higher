446065dd0bb6fa93e1a6f67955f265aad7664fc2
Lev Gorodinski <eulerfx@gmail.com>
2015-09-20 22:54:15 -0400

    fixedpoint list poc

--- src/Higher.Core/Fix.fs
+++ src/Higher.Core/Fix.fs
@@ -3,6 +3,14 @@
 /// Fix point.
 type Fix<'F> = Fix of App<'F, Fix<'F>>
 
+type Fix private () =
+  static let token = new Fix()
+  static member Inj (value : Fix<'F>) : App<Fix, 'F> = 
+      new App<_, _>(token, value)
+  static member Prj (app : App<Fix, 'F>) : Fix<'F> = 
+      app.Apply(token) :?> _
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Fix =
   
   let inline un (Fix(f)) = f
@@ -21,5 +29,4 @@ module Fix =
 
   let hylo (func : Functor<'F>) (alg : Algebra<'F, 'B>) (coalg : CoAlgebra<'F, 'A>) (a : 'A) : 'B =    
     ana func coalg a |> cata func alg
-
-  
+        
